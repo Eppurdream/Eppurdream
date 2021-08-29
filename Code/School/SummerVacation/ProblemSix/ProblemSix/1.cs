6fb4e36d639332d6a4e9c90b104ade605c1b2c9f
김민준 <eppurdream@gmail.com>
2021-08-29 23:23:07 +0900

    여름방학 여섯번째 문제

---
+++ Code/School/SummerVacation/ProblemSix/ProblemSix/1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+
+
+namespace ProblemSix
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    string s = ReadLine();
+
+        //    WriteLine(Func(s));
+        //}
+
+        // [문제 01] JadenCase 문자열 만들기
+        // JadenCase란 모든 단어의 첫 문자가 대문자이고, 그 외의 알파벳은 소문자인 문자열입니다.
+        // 문자열 s가 주어졌을 때, s를 JadenCase로 바꾼 문자열을 return 하는 함수를 완성해주세요.
+        // (s는 길이 1 이상인 문자열입니다. s는 알파벳과 공백문자로 이루어져 있습니다. 첫 문자가 영문이 아닐 때는 이어지는 영문은 소문자로 씁니다.
+
+        static string Func(string s)
+        {
+            return s[0] == ' ' ? s.ToLower() : Convert.ToChar(s.ToLower()[0] - 32) + s.Remove(0, 1).ToLower();
+        }
+    }
+}
---
+++ Code/School/SummerVacation/ProblemSix/ProblemSix/3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+
+
+namespace ProblemSix
+{
+    class _3
+    {
+        //static void Main(string[] args)
+        //{
+        //    Write("길이 입력 : ");
+        //    int length = int.Parse(ReadLine());
+
+        //    int[] prices = new int[length];
+
+        //    for(int i = 0; i < length; i++)
+        //    {
+                
+        //        prices[i] = int.Parse(ReadLine());
+        //    }
+
+        //    foreach(string str in Solution(prices))
+        //    {
+        //        WriteLine(str);
+        //    }
+        //}
+
+        // [문제 03] 주식 가격
+        // 초 단위로 기록된 주식가격이 담긴 배열 prices가 매개변수로 주어질 때,
+        // 가격이 떨어지지 않은 기간은 몇 초인지를 return 하도록 solution 함수를 완성하세요.
+        // (prices의 각 가격은 1 이상 10,000 이하인 자연수입니다. prices의 길이는 2 이상 100,000 이하입니다.)
+
+        static string[] Solution(int[] prices)
+        {
+            List<string> result = new List<string>();
+
+            for(int i = 0; i < prices.Length; i++)
+            {
+                for(int n = i; n < prices.Length; n++)
+                {
+                    if(prices[i] > prices[n])
+                    {
+                        result.Add($"{i + 1}초 시점의 ${prices[i]}은 {n - i}초 뒤에 가격이 떨어집니다.");
+                        break;
+                    }
+                    if(n + 1 == prices.Length)
+                    {
+                        result.Add($"{i + 1}초 시점의 ${prices[i]}은 끝까지 가격이 떨어지지 않았습니다.");
+                        break;
+                    }
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
